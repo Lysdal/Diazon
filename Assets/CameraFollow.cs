4329bf22397a0c5c83621bfb9588751d46187f51
Jesper Lysdal <jesper@lysdals.dk>
2018-01-12 19:21:51 +0100

    added following camera

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate ()
+	{
+	    transform.position = player.transform.position;
 	}
 }
